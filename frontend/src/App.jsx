706f09040efb06f73e0020f7a1ca55761f5ba746
sid@747 <sidmm747@gmail.com>
2024-05-18 00:07:00 +0530

    added Footer and custom bootstrap theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Header } from './components/Header'
 import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+
+
 const App = () => {
   return (
     <>
       <Header />
+
       <main className='py-3'>
         <Container>
           <h1>Welcome to Shop</h1>
         </Container>
       </main>
+
+      <Footer />
     </>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,12 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa'
 export const Header = () => {
     return (
         <header>
-            <Navbar bg="dark" variant='dark' expand="md" collapseOnSelect>
+            <Navbar bg="primary" variant='dark' expand="md" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='/'>Albatross</Navbar.Brand>
+                    <Navbar.Brand href='/'>SidShop</Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className='ms=auto mx-5    '>
+                        <Nav className='ms-auto'>
                             <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
                             <Nav.Link href='/login'><FaUser /> Sign In</Nav.Link>
                         </Nav>
