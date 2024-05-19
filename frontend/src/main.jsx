600c1650f61179b9f4836af0ed9928a3bdeccea3
sid@747 <sidmm747@gmail.com>
2024-05-19 20:40:40 +0530

    Adding Wrapper to Route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import "bootstrap/dist/css/bootstrap.min.css"
 import "./assets/styles/index.css"
 import "./assets/styles/bootstrap.custom.css"
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
 
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
