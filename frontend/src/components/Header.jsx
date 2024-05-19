85c6ffa75941c61c96ce26bf61c24f418e185544
sid@747 <sidmm747@gmail.com>
2024-05-19 23:51:43 +0530

    Added Rating Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ export const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className='ms-auto'>
-                            
+
                             <LinkContainer to="/cart">
                                 <Nav.Link>
                                     <FaShoppingCart />  Cart
