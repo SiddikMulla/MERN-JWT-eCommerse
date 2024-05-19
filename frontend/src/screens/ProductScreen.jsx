54e8a016525aee1d2598c6c7b208d1814f798879
sid@747 <sidmm747@gmail.com>
2024-05-20 01:28:13 +0530

    Added Single product Pages

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import products from '../products'
+import { HiArrowCircleLeft } from "react-icons/hi";
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import Swal from 'sweetalert2'
+
+const ProductScreen = () => {
+    const swal = () => {
+        Swal.fire({
+            title: "Added!",
+            text: "Product Added to Cart!",
+            icon: "success"
+        });
+    }
+    const { id: productId } = useParams();
+    const product = products.find((p) => p._id === productId);
+    console.log(product)
+
+    return (
+
+        <>
+            <Link className='btn btn-light mb-3' to="/">
+                <HiArrowCircleLeft /> Go Back
+            </Link>
+
+            <Row>
+                <Col md={5}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+
+                <Col md={4}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Price:${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Desciption:{product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>${product.price}</Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>{product.countInStock > 0 ? "In the stock" : "Out of Stock"}</Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button
+                                    className='btn-block'
+                                    type='submit'
+                                    disabled={product.countInStock === 0}
+                                    onClick={swal}
+                                >
+                                    Add to Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ProductScreen
