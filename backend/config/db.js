6c9614d33b65d8975b710f7506a16cb9bd1ca745
sid@747 <sidmm747@gmail.com>
2024-05-21 22:04:04 +0530

    Added @import & @destroy

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const connectDb = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected:${conn.connection.host}`);
+        
 
     } catch (error) {
         console.log(`Error ${error.message}`);
--- backend/data/users.js
+++ backend/data/users.js
@@ -20,3 +20,5 @@ const user = [
         isAdmin: false,
     },
 ]
+
+export default user;
