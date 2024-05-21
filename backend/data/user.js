37d82be7e4a634713b80715afc464f597dbcaea4
sid@747 <sidmm747@gmail.com>
2024-05-21 20:14:09 +0530

    Added Users and products

---
+++ backend/data/user.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const user = [
+    {
+        name: 'Admin User',
+        email: 'admin@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    },
+]
