1b2878fd21aa1377c70999eb2e7920d324061b4d
sid@747 <sidmm747@gmail.com>
2024-05-23 11:28:14 +0530

    Added Model to routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,22 @@
 import express from 'express';
-import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 const router = express.Router();
+import Product from '../models/productModel.js';
 
-router.get('/', (req, res) => {
-    res.json(products);
-});
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}));
 
-router.get('/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product);
+    }
+
+    res.status(404).json({message:'product Not found'})
+}));
 
 
 export default router;
