e41fc4195fdbb5fb7d261242ae9705e67745c014
sid@747 <sidmm747@gmail.com>
2024-05-24 20:13:06 +0530

    Added Slice

---
+++ frontend/slices/apiSlice.js
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../src/constant';
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL })
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: ['Product', 'Order', 'User'],
+    endpoints: (builder) => ({}),
+})
