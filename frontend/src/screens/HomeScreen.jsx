62880b2ed43df22e12953c1b7aec5aebf0abd83c
sid@747 <sidmm747@gmail.com>
2024-05-25 18:03:39 +0530

    fetched data from @Redux Slice

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react'
 import { Row, Col } from "react-bootstrap"
 import ProductList from '../components/ProductList'
-import axios from 'axios';
+import { useGetProductsQuery } from "../../slices/productApiSlice.js"
 
 const HomeScreen = () => {
-
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        };
-        fetchProducts();
-
-    }, [])
+    const { data: products, isLoading, error } = useGetProductsQuery();
 
     return (
         <>
-            <div>
-                <h3 style={{ textAlign: 'center' }}>Latest Products</h3>
-                <Row>
-                    {products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <ProductList product={product} />
-                        </Col>
-                    ))}
-                </Row>
-            </div>
+            {isLoading ? (
+                <h1>Loading...</h1>
+            ) : error ? (
+                <div>{error?.data?.message || error.error}</div>
+            ) :
+                (<div>
+                    <h3 style={{ textAlign: 'center' }}>Latest Products</h3>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <ProductList product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </div>)}
+
+
         </>
     )
 }
