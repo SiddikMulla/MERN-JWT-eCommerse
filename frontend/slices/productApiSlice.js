38319a9011b38251348160ed482e4c9769e17463
sid@747 <sidmm747@gmail.com>
2024-05-26 12:16:19 +0530

    added slice for product

--- frontend/slices/productApiSlice.js
+++ frontend/slices/productApiSlice.js
@@ -1,15 +1,21 @@
-import { PRODUCT_URL } from "../src/constant";
-import { apiSlice } from "./apiSlice";
+import { PRODUCT_URL } from '../src/constant';
+import { apiSlice } from './apiSlice';
 
-export const productApiSlice = apiSlice.injectEndpoints({
+export const productSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => ({
                 url: PRODUCT_URL,
             }),
-            keepUnusedDataFor: 5
-        })
+            keepUnusedDataFor: 5,
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCT_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
     }),
 });
 
-export const { useGetProductsQuery } = productApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productSlice;
