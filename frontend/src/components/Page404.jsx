4207b307386541ee2e612c4a7246ce619f9db22a
sid@747 <sidmm747@gmail.com>
2024-05-28 20:30:40 +0530

    Button

--- frontend/src/components/Page404.jsx
+++ frontend/src/components/Page404.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import img from '../assets/404.png'
 import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 export const Page404 = () => {
   return (
@@ -9,8 +9,8 @@ export const Page404 = () => {
       <div style={{ textAlign: 'center' }}>
 
         <img src={img} alt="" className='img-fluid' />
-
-        <Link to='/' style={{ color: 'Green ' }}><p className="h3">Click to Back Home</p></Link>
+        <br />
+        <Link to='/' style={{ color: 'Green', textDecoration: 'none' }}><Button variant="outline-primary ">Back to Home</Button></Link>
       </div>
     </>
   )
