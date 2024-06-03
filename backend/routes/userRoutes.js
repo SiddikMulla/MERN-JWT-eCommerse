3b2259fdfaca2c3b8bb115694684acf133f2aa3b
sid@747 <sidmm747@gmail.com>
2024-06-03 19:00:24 +0530

    Created routes for user Activity

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const router = express.Router();
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    updateUserProfile,
+    getUserProfile,
+    getUsers,
+    getUsersById,
+    deleteUsers,
+    updateUser
+} from '../controller/userController.js'
+
+router.route("/").post(registerUser).get(getUsers);
+
+router.post("/logout", logoutUser);
+
+router.post("/login", authUser);
+
+router.route("/").get(getUserProfile).put(updateUserProfile);
+
+
+router.route("/:id").get(getUsersById).delete(deleteUsers).put(updateUser);
+
+
