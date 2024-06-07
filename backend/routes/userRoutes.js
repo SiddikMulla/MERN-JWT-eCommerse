123239cfbdca8755466297999f9eaf8d5e4185f4
SiddikMulla <sidmm747@gmail.com>
2024-06-07 15:11:49 +0530

    staged middleware to routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,14 +12,15 @@ import {
     updateUser
 } from '../controller/userController.js'
 
+import { protectRoutes, admin } from '../middleware/authMiddleware.js';
+
 router.route("/").post(registerUser).get(getUsers);
 
 router.post("/logout", logoutUser);
 
 router.post("/login", authUser);
 
-router.route("/").get(getUserProfile).put(updateUserProfile);
-
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 router.route("/:id").get(getUsersById).delete(deleteUsers).put(updateUser);
 
