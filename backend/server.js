01354a29c4f2dee7bb4c90f370fc8881c69ecdb1
SiddikMulla <sidmm747@gmail.com>
2024-06-07 12:16:13 +0530

    cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDb from './config/db.js';
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDb();//Mongo Connection
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 app.use('/api/products', productRoutes)
 
 app.use('/api/users', userRoutes)
