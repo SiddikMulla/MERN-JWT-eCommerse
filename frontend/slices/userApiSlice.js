2bc26740497b19ac0cef1bf16f28944bff5f4d06
SiddikMulla <sidmm747@gmail.com>
2024-06-08 23:51:54 +0530

    created UserApiSlice State

---
+++ frontend/slices/userApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from '../src/constant';
+import { apiSlice } from './apiSlice';
+
+export const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: USERS_URL / auth,
+                method: 'post',
+                body: data,
+            }),
+        }),
+
+    }),
+});
+
+export const { useLoginMutation, } = userApiSlice;
