c84803a6ec2daa495b0da08aaeae7cda3862e670
SiddikMulla <sidmm747@gmail.com>
2024-06-08 23:45:36 +0530

    Created authSlice state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
-import  cartSliceReducer from "../slices/cartSlice.js";
-
+import cartSliceReducer from "../slices/cartSlice.js";
+import authSliceReducer from "../slices/authSlice.js";
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        cart:cartSliceReducer,
+        cart: cartSliceReducer,
+        auth: authSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat
         (apiSlice.middleware),
