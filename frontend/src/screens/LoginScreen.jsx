25f6e06e27f233ebd6ce1c00379cedf56c0cc86a
SiddikMulla <sidmm747@gmail.com>
2024-06-09 16:19:02 +0530

    auth to login

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,57 +1,88 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import FormContainer from '../components/FormContainer'
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+// import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+
+import { useLoginMutation } from '../../slices/userApiSlice';
+import { setCredentials } from '../../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const submitHandler = (e) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const { search } = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect') || '/';
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log("submit")
-    }
-
+        try {
+            const res = await login({ email, password }).unwrap();
+            dispatch(setCredentials({ ...res }));
+            navigate(redirect);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
 
     return (
         <FormContainer>
             <h1>Sign In</h1>
-            <Form onSubmit={submitHandler} >
-                <Form.Group controlId='email' className='my-3'>
-                    <Form.Label>Email</Form.Label>
+
+            <Form onSubmit={submitHandler} className='d-flex row'>
+                <Form.Group className='my-2' controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
                     <Form.Control
                         type='email'
                         placeholder='abc@gmail.com'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        required
-                    >
-                    </Form.Control>
+                    ></Form.Control>
                 </Form.Group>
 
-                <Form.Group controlId='password' className='my-3'>
+                <Form.Group className='my-2' controlId='password'>
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                         type='password'
-                        placeholder='*******'
+                        placeholder='**********'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        required
-                    >
-                    </Form.Control>
+                    ></Form.Control>
                 </Form.Group>
 
-                <Button type='submit' variant='primary' className='mt-3'>
+                <Button disabled={isLoading} type='submit' variant='dark' className='mt-3'>
                     Sign In
                 </Button>
+
+              
             </Form>
+
             <Row className='py-3'>
                 <Col>
-                    New Customer? <Link to='/register'>Register</Link>
+                    New Customer?{' '}
+                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+                        Register
+                    </Link>
                 </Col>
             </Row>
         </FormContainer>
-    )
-}
+    );
+};
 
-export default LoginScreen
+export default LoginScreen;
