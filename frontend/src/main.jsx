16f08f7699615c8df8f2a284711a5edc795eb4fc
SiddikMulla <sidmm747@gmail.com>
2024-06-12 23:07:20 +0530

    Shifted to private Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,19 +21,22 @@ import RegisterScreen from './screens/RegisterScreen.jsx'
 import ShippingScreen from './screens/ShippingScreen.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import PaymentScreen from './screens/PaymentScreen.jsx'
+import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/product/:id' element={<ProductScreen />} />
-      <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='/*' element={<Page404 />} />
+      
       <Route path='' element={<PrivateRoute />} >
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route index={true} path='/' element={<HomeScreen />} />
+        <Route path='/product/:id' element={<ProductScreen />} />
+        <Route path='/cart' element={<CartScreen />} />
       </Route>
     </Route>
   )
