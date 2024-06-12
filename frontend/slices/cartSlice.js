37c3d894c25affd8cc5b8fb3047a725a38f9e145
SiddikMulla <sidmm747@gmail.com>
2024-06-12 23:05:34 +0530

    clearCartItem slice

--- frontend/slices/cartSlice.js
+++ frontend/slices/cartSlice.js
@@ -37,8 +37,12 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       localStorage.setItem('cart', JSON.stringify(state));
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return cartPricing(state)
+    }
   },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod, clearCartItems } = cartSlice.actions;
 export default cartSlice.reducer;
